90b13ef5368441909399070c742cabdece451037
c91s1h8 <csh9118>
2017-06-05 17:19:57 +0800

    2

--- code/fcWebsite/src/main/java/com/feelcolor/website/controller/HomeController.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/controller/HomeController.java
@@ -1,17 +1,14 @@
 package com.feelcolor.website.controller;
 
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    
+    /**
+     * 首页
+     * @return
+     */
     @RequestMapping("/")
     public String home(){
         return "home";
