0bf2ee163a4f4c541a2fff79d043faffa7ef8e3f
c91s1h8 <csh9118>
2017-06-05 17:18:40 +0800

    1

--- code/fcWebsite/src/main/java/com/feelcolor/website/controller/HomeController.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/controller/HomeController.java
@@ -1,20 +1,19 @@
 package com.feelcolor.website.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-    @RequestMapping("/")
-    public String index(){
-        return "index";
-    }
     
-    @RequestMapping("/test")
-    public String test(){
-        System.out.println(LocaleContextHolder.getLocale());
-        return "index";
+    @RequestMapping("/")
+    public String home(){
+        return "home";
     }
 }
--- code/fcWebsite/src/main/java/com/feelcolor/website/controller/TestController.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/controller/TestController.java
@@ -1,13 +1,25 @@
 package com.feelcolor.website.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    
-    
+    @Autowired
+    private MessageSource messageSource;
 
-    public String i18(){
-        return "";
+    
+    @RequestMapping("/test")
+    public String test(Model model){
+        Locale locale = LocaleContextHolder.getLocale();
+        //后台获取国际化值方式
+        model.addAttribute("hello", messageSource.getMessage("hello", null, locale));
+        return "index";
     }
 }
