7585b155ebaa08730949af44e0bd645114694721
c91s1h8 <csh9118>
2017-06-05 15:20:54 +0800

    2

--- code/fcWebsite/src/main/java/com/feelcolor/website/FcWebsiteApplication.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/FcWebsiteApplication.java
@@ -1,9 +1,13 @@
 package com.feelcolor.website;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement    //开启事物
+@MapperScan("com.feelcolor.website.dao.mapper")    //Mybatis mapper扫描2
 public class FcWebsiteApplication {
 
 	public static void main(String[] args) {
