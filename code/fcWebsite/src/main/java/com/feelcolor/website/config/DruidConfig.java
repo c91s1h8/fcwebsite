584e918ab21d2bcd50da22f5c1db343b398cceae
c91s1h8 <csh9118>
2017-06-09 19:24:11 +0800

    2

--- code/fcWebsite/src/main/java/com/feelcolor/website/config/DruidConfig.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/config/DruidConfig.java
@@ -1,23 +1,23 @@
-package com.feelcolor.website.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import lombok.Data;
-
-/**
- * 将properties属性注入到bean中
- * @author Administrator
- *
- */
-@ConfigurationProperties(prefix="druid")    //自动注入properties中druid开头的属性到变量中
-@Data
-public class DruidConfig {
-
-    private String url;
-    private String username;
-    private String password;
-    private int maxActive;
-    private int minIdle;
-    private int initialSize;
-    private boolean testOnBorrow;
-}
+//package com.feelcolor.website.config;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//import lombok.Data;
+//
+///**
+// * 将properties属性注入到bean中
+// * @author Administrator
+// *
+// */
+//@ConfigurationProperties(prefix="druid")    //自动注入properties中druid开头的属性到变量中
+//@Data
+//public class DruidConfig {
+//
+//    private String url;
+//    private String username;
+//    private String password;
+//    private int maxActive;
+//    private int minIdle;
+//    private int initialSize;
+//    private boolean testOnBorrow;
+//}
