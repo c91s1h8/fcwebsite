1d65521eca1aa53820541892b975bccaa7927208
c91s1h8 <csh9118>
2017-06-16 16:59:39 +0800

    1

--- code/fcWebsite/src/main/java/com/feelcolor/website/controller/UserInfoController.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/controller/UserInfoController.java
@@ -2,6 +2,7 @@ package com.feelcolor.website.controller;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.feelcolor.website.model.po.UserInfo;
 import com.feelcolor.website.service.UserInfoService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("user")
 public class UserInfoController {
@@ -17,6 +21,8 @@ public class UserInfoController {
 
     @RequestMapping("/getUserById")
     public @ResponseBody UserInfo getUserById(String id){
+        log.info("cccccccccccccccccccccccccccccccccccccccccccccccc");
+        log.info("ddddddddddddddddddddddddddddddddddddddddddddddddd");
         return userInfoService.selectByPrimaryKey(id);
     }
 }
