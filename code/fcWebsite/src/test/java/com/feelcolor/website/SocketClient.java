aa0a225d68f3629464028f9ce37a10f5746c9aa6
c91s1h8 <csh9118>
2017-07-21 13:43:09 +0800

    1

--- code/fcWebsite/src/test/java/com/feelcolor/website/SocketClient.java
+++ code/fcWebsite/src/test/java/com/feelcolor/website/SocketClient.java
@@ -4,14 +4,17 @@ import java.io.*;
 import java.net.Socket;
 
 public class SocketClient {
+    private static final String host = "192.168.2.183";
+    private static final int port = 10088;
+
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("192.168.2.183", 10088);
+        Socket socket = new Socket(host, port);
         if (socket.isConnected()) {
             InputStream is;
             OutputStream os;
-            while (true){
+            while (true) {
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                String in  =br.readLine();
+                String in = br.readLine();
                 os = socket.getOutputStream();
                 os.write(in.getBytes());
                 os.flush();
@@ -19,7 +22,7 @@ public class SocketClient {
                 is = socket.getInputStream();
                 byte[] tmp = new byte[2000];
                 is.read(tmp);
-                System.out.println("客户端收到返回消息:"+new String(tmp));
+                System.out.println("客户端收到返回消息:" + new String(tmp));
 
 //                is.close();
 //                os.close();
--- code/fcWebsite/src/test/java/com/feelcolor/website/SocketServer.java
+++ code/fcWebsite/src/test/java/com/feelcolor/website/SocketServer.java
@@ -7,23 +7,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServer {
+    private static final int port = 10088;
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(10088);
-        Socket socket = serverSocket.accept();
-        while (true){
-            System.out.println("1111111111111111111111111");
-            if(socket!=null){
-                InputStream is =null;
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        while (true) {
+            System.out.println("等待客户端链接");
+            Socket socket = serverSocket.accept();
+            if (socket != null) {
+                InputStream is = null;
                 OutputStream os = null;
                 String ip = socket.getInetAddress().toString();
                 System.out.println(ip);
 
-                is=socket.getInputStream();
-                os=socket.getOutputStream();
+                is = socket.getInputStream();
+                os = socket.getOutputStream();
                 byte[] tmp = new byte[2000];
                 is.read(tmp);
 
-                os.write(("服务器接收到发送消息："+new String(tmp)).getBytes());
+                os.write(("服务器接收到发送消息：" + new String(tmp)).getBytes());
 
                 is.close();
                 os.close();
