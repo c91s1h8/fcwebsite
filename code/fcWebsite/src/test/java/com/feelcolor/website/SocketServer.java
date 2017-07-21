6d1e4d7215d4e3641cf11cac56a143bb93dccabd
c91s1h8 <csh9118>
2017-07-21 10:38:54 +0800

    1

--- code/fcWebsite/src/test/java/com/feelcolor/website/SocketServer.java
+++ code/fcWebsite/src/test/java/com/feelcolor/website/SocketServer.java
@@ -1,4 +1,13 @@
 package com.feelcolor.website;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SocketServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1000);
+        Socket socket = serverSocket.accept();
+    }
+
 }
