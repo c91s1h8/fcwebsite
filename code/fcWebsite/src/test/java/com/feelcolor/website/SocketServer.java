e3a608e2eb9c10c8d206a4240142aa4f1f17516d
c91s1h8 <csh9118>
2017-07-21 11:00:01 +0800

    1

--- code/fcWebsite/src/test/java/com/feelcolor/website/SocketServer.java
+++ code/fcWebsite/src/test/java/com/feelcolor/website/SocketServer.java
@@ -1,13 +1,24 @@
 package com.feelcolor.website;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(1000);
+        ServerSocket serverSocket = new ServerSocket(10086);
         Socket socket = serverSocket.accept();
+        while (true){
+            System.out.println("1111111111111111111111111");
+            if(socket!=null){
+                InputStream is =null;
+                OutputStream os = null;
+                String ip = socket.getInetAddress().toString();
+                System.out.println(ip);
+            }
+        }
     }
 
 }
