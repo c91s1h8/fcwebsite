e7673d59819f4153c9511ad1f566877849404943
c91s1h8 <csh9118>
2017-09-07 15:45:20 +0800

    -

--- code/fcWebsite/src/main/java/com/feelcolor/website/dao/mapper/UserInfoMapper.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/dao/mapper/UserInfoMapper.java
@@ -1,9 +1,6 @@
 package com.feelcolor.website.dao.mapper;
 
 import com.feelcolor.website.model.po.UserInfo;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 public interface UserInfoMapper {
     int deleteByPrimaryKey(String id);
@@ -17,9 +14,4 @@ public interface UserInfoMapper {
     int updateByPrimaryKeySelective(UserInfo record);
 
     int updateByPrimaryKey(UserInfo record);
-
-    Integer getTotalRecord(@Param("id") Integer id, @Param("name") String name, @Param("age") Integer age);
-
-    List<UserInfo> selectWithPage(@Param("id") Integer id, @Param("name") String name, @Param("age") Integer age,
-            @Param("pageStart") int offset, @Param("pageSize") int pageSize);
 }
--- code/fcWebsite/src/main/java/com/feelcolor/website/model/po/UserInfo.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/model/po/UserInfo.java
@@ -1,17 +1,175 @@
 package com.feelcolor.website.model.po;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Date;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserInfo {
     private String id;
 
-    private String name;
+    private String account;
 
-    private Integer age;
+    private String password;
 
+    private String mobile;
+
+    private String email;
+
+    private String nickName;
+
+    private Integer sex;
+
+    private String provinceCode;
+
+    private String cityCode;
+
+    private String countyCode;
+
+    private Float lng;
+
+    private Float lat;
+
+    private String registerIp;
+
+    private String registerSource;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private Date lastUpdateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account == null ? null : account.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName == null ? null : nickName.trim();
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode == null ? null : provinceCode.trim();
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode == null ? null : cityCode.trim();
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode == null ? null : countyCode.trim();
+    }
+
+    public Float getLng() {
+        return lng;
+    }
+
+    public void setLng(Float lng) {
+        this.lng = lng;
+    }
+
+    public Float getLat() {
+        return lat;
+    }
+
+    public void setLat(Float lat) {
+        this.lat = lat;
+    }
+
+    public String getRegisterIp() {
+        return registerIp;
+    }
+
+    public void setRegisterIp(String registerIp) {
+        this.registerIp = registerIp == null ? null : registerIp.trim();
+    }
+
+    public String getRegisterSource() {
+        return registerSource;
+    }
+
+    public void setRegisterSource(String registerSource) {
+        this.registerSource = registerSource == null ? null : registerSource.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
 }
