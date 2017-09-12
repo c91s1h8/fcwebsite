6befc16b16eb656e6130d4676973c43b256c6d93
c91s1h8 <csh9118>
2017-09-12 14:46:22 +0800

    -

--- code/fcWebsite/src/main/java/com/feelcolor/website/FcWebsiteApplication.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/FcWebsiteApplication.java
@@ -3,12 +3,25 @@ package com.feelcolor.website;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 @EnableTransactionManagement    //开启事物
 @MapperScan("com.feelcolor.website.dao.mapper")    //Mybatis mapper扫描2
 public class FcWebsiteApplication {
+
+	@Resource
+	private RestTemplateBuilder builder;
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return builder.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FcWebsiteApplication.class, args);
--- code/fcWebsite/src/main/java/com/feelcolor/website/controller/SSLController.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/controller/SSLController.java
@@ -2,13 +2,29 @@ package com.feelcolor.website.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("ssl")
 public class SSLController {
+    @Resource
+    private RestTemplate restTemplate;
 
     @RequestMapping("/ssltest")
+    @ResponseBody
     public String ssltest(){
         return "ok,ssl";
     }
+
+    @RequestMapping("/sslpost")
+    @ResponseBody
+    public String sslpost(){
+        String rs = restTemplate.postForObject("",null,String.class);
+        return rs;
+    }
+
+
 }
