df9b5ca91f9c1d749549d8df20cdd6f655cf959c
liangxiaojin <1287515816@qq.com>
2018-02-07 17:20:56 +0800

    -

--- code/fcWebsite/src/main/java/com/feelcolor/website/common/wx/SignUtil.java
+++ code/fcWebsite/src/main/java/com/feelcolor/website/common/wx/SignUtil.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class SignUtil {
     // 与接口配置信息中的Token要一致
-    private static String token = "weixinmp";
+    private static String token = "weixincsh";
 
     /**
      * 验证签名
