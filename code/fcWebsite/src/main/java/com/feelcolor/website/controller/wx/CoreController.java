c432e0cb30e8b6f905036a139a4e8469ffb77334
liangxiaojin <1287515816@qq.com>
2018-02-07 16:56:12 +0800

    redislock

---
+++ code/fcWebsite/src/main/java/com/feelcolor/website/controller/wx/CoreController.java
@@ -0,0 +1,29 @@
+package com.feelcolor.website.controller.wx;
+
+import com.feelcolor.website.common.wx.SignUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+@Slf4j
+public class CoreController {
+    //验证是否来自微信服务器的消息
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public String checkSignature(@RequestParam(name = "signature" ,required = false) String signature  ,
+                                 @RequestParam(name = "nonce",required = false) String  nonce ,
+                                 @RequestParam(name = "timestamp",required = false) String  timestamp ,
+                                 @RequestParam(name = "echostr",required = false) String  echostr){
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+            log.info("接入成功");
+            return echostr;
+        }
+        log.error("接入失败");
+        return "";
+    }
+
+}
